35eba9ed19b60bfcc393a97b00fd28559c1f128d
John Ferrry <johntferry@gmail.com>
2020-02-18 01:46:19 +0000

    Feb 19 edits

--- dice.rb
+++ dice.rb
@@ -1,8 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 def view(template); erb template.to_sym; end
-before { puts "Parameters: #{params}" }  
+before { puts "Parameters: #{params}" }
 
 get "/" do
-  "Hello"
+  "Hello, world!"
 end
