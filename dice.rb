ac10899ed5edac3302a3403b17ba4f5a2f1e715d
John Ferrry <johntferry@gmail.com>
2020-02-23 22:45:43 +0000

    Commit 3

--- dice.rb
+++ dice.rb
@@ -1,10 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
 def view(template); erb template.to_sym; end
-before { puts "Parameters: #{params}" }  
+before { puts "Parameters: #{params}" }
 
 get "/" do
+<<<<<<< HEAD
     @die1 = rand(1..6)
     @die2 = rand(1..6)
     view "dice"
+=======
+  "Hello, world!"
+>>>>>>> 35eba9ed19b60bfcc393a97b00fd28559c1f128d
 end
